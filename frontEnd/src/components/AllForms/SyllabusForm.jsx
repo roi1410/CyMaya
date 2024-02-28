0b465bf4e42b3696f03f3b1e0e460d474abc65cf
roi <roi14102000@gmail.com>
2024-02-28 12:43:00 +0200

    changing

--- frontEnd/src/components/AllForms/SyllabusForm.jsx
+++ frontEnd/src/components/AllForms/SyllabusForm.jsx
@@ -12,16 +12,23 @@ function SyllabusForm({ register, control }) {
     <>
       <AppendFields append={append} />
 
-      <div className="  max-h-full ">
         <label className=" text-sm font-medium text-gray-900 dark:text-gray-300">
-          haw mach time the titled went?
+          Course Name
         </label>
         <input
-          defaultValue={"period"}
+          
           className="block w-20 border disabled:cursor-not-allowed disabled:opacity-50 bg-gray-50 border-gray-300 text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500 rounded-lg p-2.5 text-sm"
-          {...register("dsg")}
+          {...register("courseName")}
         />
-      </div>
+        <label className=" text-sm font-medium text-gray-900 dark:text-gray-300">
+          time ?
+        </label>
+        <input
+          
+          className="block w-20 border disabled:cursor-not-allowed disabled:opacity-50 bg-gray-50 border-gray-300 text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500 rounded-lg p-2.5 text-sm"
+          {...register("hours")}
+        />
+     
     </>
   );
 }
