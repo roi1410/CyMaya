3380e4da43d894b7e58c1176688ded45c4b3bdb9
roi <roi14102000@gmail.com>
2024-02-28 12:17:11 +0200

    new shit

--- frontEnd/src/components/Pages/FormsPage.jsx
+++ frontEnd/src/components/Pages/FormsPage.jsx
@@ -1,14 +1,19 @@
 import React from 'react'
 import CourseForm from '../AllForms/CourseForm';
 import { useForm } from "react-hook-form";
+import SyllabusForm from '../AllForms/SyllabusForm';
+
 
 function FormsPage() {
     
-    const { register, handleSubmit } = useForm();
+    const { register, handleSubmit,control } = useForm();
     return (
-      <form onSubmit={handleSubmit((data)=>console.log(data))} className=" bg-primary">
+      <form onSubmit={handleSubmit((data)=>console.log(data))} className=" bg-primary ring-4 ">
         
-        <CourseForm register={register}/>
+        {/* <CourseForm register={register}/> */}
+       <SyllabusForm />
+        
+      
      
       </form>
   )
